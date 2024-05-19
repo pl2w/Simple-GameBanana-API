ba53fb2ab9f6e9031f3129a26f850dd8242656dd
pl2w <dc.pl2w@gmail.com>
2024-05-19 11:59:38 +0300

    credits

---
+++ GameBananaAPI/Data/AuthorData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameBananaAPI.Data
+{
+    public class AuthorData
+    {
+        public string _sRole {  get; set; }
+        public string _sName { get; set; }
+    }
+}
---
+++ GameBananaAPI/Data/CreditsData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace GameBananaAPI.Data
+{
+    public class CreditsData
+    {
+        public string _sGroupName { get; set; }
+        public List<AuthorData> _aAuthors { get; set; }
+    }
+}
